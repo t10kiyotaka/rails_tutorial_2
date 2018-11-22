9ec58c5e0d14dd6386a0938828cf395f3c879680
Kiyotaka <>
2018-11-22 16:29:50 +0900

    refactor User#feed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -97,5 +97,23 @@ class UserTest < ActiveSupport::TestCase
     assert_not michael.following?(archer)
   end
 
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+    # Check the following user's posts
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Check my own posts
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Check the not following user's post
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+
 
 end
